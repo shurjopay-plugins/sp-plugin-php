9b201539c146010a528e3f97d80ba94b69802ab8
Imtiaz Rahi <imtiaz.rahi@gmail.com>
2023-01-17 20:34:12 +0600

    successfully used Shurjopay and Config class in test

--- src/ShurjopayException.php
+++ src/ShurjopayException.php
@@ -1,7 +1,7 @@
 <?php
+
 namespace ShurjopayPlugin;
 
-// use Exception;
 use Throwable;
 
 /**
@@ -13,34 +13,38 @@ use Throwable;
  * @author Rayhan Khan Ridoy
  * @since 2022-12-01
  */
- class ShurjopayException extends \Exception
- {
-  // Redefine the exception so message isn't optional
-  public function __construct($message, $code = 0,$e=null, Throwable $previous = null) {
-    $this->customFunction();
-    $this->exceptionInfo($message,$e);
-    // make sure everything is assigned properly
-    parent::__construct($message, $code, $previous);
+class ShurjopayException extends \Exception
+{
+    // Redefine the exception so message isn't optional
+    public function __construct($message, $code = 0, $e = null, Throwable $previous = null)
+    {
+        $this->customFunction();
+        $this->exceptionInfo($message, $e);
+        // make sure everything is assigned properly
+        parent::__construct($message, $code, $previous);
 
-}
+    }
 
 // This is a method which prints a default exception for every custom exception message.
-public function customFunction() {
-    echo "<u> <br> <h3> A ShurjoPay Message For This Type Of Exception :- </h3>  </u>";
-}
-public function exceptionInfo($message,$e){
-
-    $System_error_msg = $e->getMessage();
-    $shurjopay_help=$message;
-    $problem_file_name = $e->getFile();
-    $problem_LineNumber= $e->getLine();
-    $myResponse=["System_error_msg" => $System_error_msg,"shurjopay_help"=>$shurjopay_help, "problem_file_name" => $problem_file_name,"problem_LineNumber"=>$problem_LineNumber];
-
-    foreach($myResponse as $attribute=>$value){
-        echo $attribute."=>".$value."<br>";
+    public function customFunction()
+    {
+        echo "<u> <br> <h3> A ShurjoPay Message For This Type Of Exception :- </h3>  </u>";
     }
-    exit;
-}
+
+    public function exceptionInfo($message, $e)
+    {
+
+        $System_error_msg = $e->getMessage();
+        $shurjopay_help = $message;
+        $problem_file_name = $e->getFile();
+        $problem_LineNumber = $e->getLine();
+        $myResponse = ["System_error_msg" => $System_error_msg, "shurjopay_help" => $shurjopay_help, "problem_file_name" => $problem_file_name, "problem_LineNumber" => $problem_LineNumber];
+
+        foreach ($myResponse as $attribute => $value) {
+            echo $attribute . "=>" . $value . "<br>";
+        }
+        exit;
+    }
 
 
 }
