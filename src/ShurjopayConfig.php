cdedc20c0206e86f1d43d8511fbf22a82b6cccec
MD WALI MOSNAD AYSHIK <Ayshikmee@gmail.com>
2023-01-18 14:06:43 +0600

    refactor config variables

--- src/ShurjopayConfig.php
+++ src/ShurjopayConfig.php
@@ -21,19 +21,7 @@ class ShurjopayConfig
 
     /** Merchant prefix used to generate order id */
     public $order_prefix;
-}
 
-// TODO remove all constant variables
-// Configuration constants for shurjopay plugin
-/** shurjopay merchant username */
-define('SP_USERNAME', 'sp_sandbox', false);
-/** shurjopay merchant password */
-define('SP_PASSWORD', 'pyyk97hu&6u6', false);
-/** Merchant prefix used to generate order id */
-define('SP_PREFIX', 'NOK', false);
-/** shurjopay payment gateway API endpoint */
-define('SHURJOPAY_API', 'https://sandbox.shurjopayment.com/', false);
-/** URL to redirect after completion of a payment */
-define('SP_CALLBACK', 'https://sandbox.shurjopayment.com/response', false);
-/** Log location of shurjopay php plugin */
-define('SP_LOG_LOCATION', 'shurjoPay-plugin-log/', false);
+    /** Merchant prefix used to generate order id */
+    public $ssl_verifypeer;
+}
